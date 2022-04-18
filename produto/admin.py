2399240e5bc84c36122f153c566dabbf8e664eae
Vinnicius M <vinniciusmartins@dpc.com.br>
2022-04-18 15:23:53 -0300

    criado os models Produto e Variação

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Produto, Variacao
 
-# Register your models here.
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 1
+    
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInline
+    ]
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Variacao)
