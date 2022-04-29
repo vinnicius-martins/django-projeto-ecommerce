caea34970bd3f81cb44299bcb3c6eb00b289dce2
vinnicius <vinniciusmartins@dpc.com.br>
2022-04-28 23:13:12 -0300

    adicionado produtos e slugs automaticos

--- produto/admin.py
+++ produto/admin.py
@@ -7,6 +7,12 @@ class VariacaoInline(admin.TabularInline):
     
 
 class ProdutoAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome', 
+        'descricao_curta', 
+        'get_preco_formatado',
+        'get_preco_promocional_formatado',
+        ]
     inlines = [
         VariacaoInline
     ]
--- produto/models.py
+++ produto/models.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
@@ -10,15 +11,23 @@ class Produto(models.Model):
     imagem = models.ImageField(upload_to='produto_imagens/%Y/%m/', 
                                blank=True,
                                null=True)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField()
-    preco_marketing_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(verbose_name='Preço')
+    preco_marketing_promocional = models.FloatField(default=0, verbose_name='Preço Promo')
     tipo = models.CharField(default='V',
                             max_length=1,
                             choices=(
-                                ('V', 'Variação'),
+                                ('V', 'Variável'),
                                 ('S', 'Simples')
                             ))
+    
+    def get_preco_formatado(self):
+        return f'R$ {self.preco_marketing:.2f}'.replace('.', ',')
+    get_preco_formatado.short_description = 'Preço'
+    
+    def get_preco_promocional_formatado(self):
+        return f'R$ {self.preco_marketing_promocional:.2f}'.replace('.', ',')
+    get_preco_formatado.short_description = 'Preço Promo.'
         
     @staticmethod
     def resize_image(img, new_width=800):
@@ -38,6 +47,10 @@ class Produto(models.Model):
             )
     
     def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.nome)}'
+            self.slug = slug
+            
         super().save(self, *args, **kwargs)
         
         max_image_size = 800
