dc7414e296b1ba3474811e733289b604bdc078bd
vinnicius <vinniciusmartins@dpc.com.br>
2022-05-20 18:14:50 -0300

    criadas urls, template listagem de produtos e detalhe do produto

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views import View
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 10
+
+
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
+
+class AdicionarAoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('AdicionarAoCarrinho')
+
+
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('RemoverDoCarrinho')
+
+
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho')
+
+
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Finalizar')
